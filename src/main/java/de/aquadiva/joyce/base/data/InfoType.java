7b0b4fc44468a893fb1ee42bd5844975ad99d1f7
khituras <chew@gmx.net>
2017-02-08 16:30:08 +0100

    Excluded julie neo4j plugins because they are currently not publicly available. Everything but download should run anyway.

--- src/main/java/de/aquadiva/joyce/base/data/InfoType.java
+++ src/main/java/de/aquadiva/joyce/base/data/InfoType.java
@@ -1,25 +1,35 @@
 package de.aquadiva.joyce.base.data;
 
 /**
- * The different types of cached information an ontology or a set of ontologies can have.
+ * The different types of cached information an ontology or a set of ontologies
+ * can have.
  * 
  * @author friederike
  * 
  */
 public enum InfoType {
 	/**
-	 * A multiset of classes defined within a given (set of) ontologie(s) that cover the input terms. 
-	 * An element is represented in the multiset as often as it occurs within the given ontologies.
+	 * A multiset of classes defined within a given (set of) ontologie(s) that
+	 * cover the input terms. An element is represented in the multiset as often
+	 * as it occurs within the given ontologies.
 	 */
 	COVERING_CLASSES,
 	/**
-	 * A multiset of classes defined within a given (set of) ontologie(s) that do not cover any input term. 
-	 * An element is represented in the multiset as often as it occurs within the given ontologies.
+	 * A multiset of classes defined within a given (set of) ontologie(s) that
+	 * do not cover any input term. An element is represented in the multiset as
+	 * often as it occurs within the given ontologies.
 	 */
 	NON_COVERING_CLASSES,
 	/**
-	 * A multiset of all the classes defined within a given (set of) ontologie(s). 
-	 * An element is represented in the multiset as often as it occurs within the given ontologies.
+	 * A multiset of all the classes defined within a given (set of)
+	 * ontologie(s). An element is represented in the multiset as often as it
+	 * occurs within the given ontologies.
 	 */
 	ALL_CLASSES,
+	/**
+	 * A multiset of classes that were found in query input text and were not
+	 * found in a given (set of) ontologie(s). Each element is only present once
+	 * since a class cannot be absent more than once.
+	 */
+	MISSING_CLASSES,
 }
